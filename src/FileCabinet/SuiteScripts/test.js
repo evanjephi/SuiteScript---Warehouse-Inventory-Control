276ead38b5acdeba2f828779b506b16ab1611106
Even Yohans <evanjephi@gmail.com>
2026-05-07 17:04:09 -0400

    testing output

--- src/FileCabinet/SuiteScripts/test.js
+++ src/FileCabinet/SuiteScripts/test.js
@@ -1,7 +1,12 @@
-       const STATUS = {
-            Hold: 4,
-            Good: 1,
-            QC: 3
-        }
+const data = [{"id":145003,"status":"Pending Fulfillment","isMainRelease":false,"items":
+    [{"item":"TDL-VIS-A","itemid":926,"qtyOrder":20,"qtyFulfilled":null,"qtyCommitted":0,"qtyPrepared":0,"prepStage":"","isStage":false,"isLineRelease":false,"commitInventory":"3","lineUniqueKey":"1513139"},{"item":"TDL-VIS-A-ALT","itemid":53771,"qtyOrder":12,"qtyFulfilled":null,"qtyCommitted":11,"qtyPrepared":0,"prepStage":"","isStage":false,"isLineRelease":false,"commitInventory":"","lineUniqueKey":"1513140"}]}]	
 
-    console.log('status ', STATUS['Hold'] + ' ' + STATUS.Good)
+data.forEach(d => {
+    const items = d.items
+    items.forEach(i => {
+        if (i.commitInventory === "3") {
+            i.isLineRelease = true
+        }
+    })
+    console.log({data: d, item: items})
+})
